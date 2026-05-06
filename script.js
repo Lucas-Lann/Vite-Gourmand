557005ae702d86448cbe37d38dff20da98fa52f4
lucas <lucas.lannoy@hotmail.fr>
2026-05-06 17:01:04 +0200

    ajout des fichier html plusimages

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+// menu burger 
+
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
